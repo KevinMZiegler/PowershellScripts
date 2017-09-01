ed11dff9e8efd29388a8bbf3672e4b5e910e85fc
Kevin Ziegler <kziegler@imoneza.com>
2017-09-01 11:17:45 -0500

    Create GetFileNameAndRowCount.ps1

---
+++ GetFileNameAndRowCount.ps1
@@ -0,0 +1,13 @@
+$Path = "[put path here]"
+$Output  = "[put output file here"
+$OutputLine 
+$outputline = "Filename | Rows"
+Write-output $outputline | out-file $output
+#Change *.csv to appropriate file type below
+ForEach ($File in (Get-ChildItem $Path -Include *.csv -Recurse))
+{
+  
+   $outputLine = $file.FullName  
+    
+   $outputline = $outputline + " | " + ((Get-Content $File.FullName |Measure-object -line).lines )
+   Write-output $outputline | out-file $output -append 
