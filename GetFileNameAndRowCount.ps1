e4dd705618bdb54ef1cd6f24f33f56827f91888e
Kevin Ziegler <kziegler@imoneza.com>
2017-11-10 16:44:46 -0600

    Update GetFileNameAndRowCount.ps1
    Added the column count to files

--- GetFileNameAndRowCount.ps1
+++ GetFileNameAndRowCount.ps1
@@ -1,15 +1,16 @@
 $Path = "[put path here]"
 $Output  = "[put output file here"
 $OutputLine 
-$outputline = "Filename | Rows"
+$outputline = "Filename | Rows | Columns"
 Write-output $outputline | out-file $output
 #Change *.csv to appropriate file type below
 ForEach ($File in (Get-ChildItem $Path -Include *.csv -Recurse))
 {
   
    $outputLine = $file.FullName  
-    
-   $outputline = $outputline + " | " + ((Get-Content $File.FullName |Measure-object -line).lines )
+   $numberOfColumns = ((($fileData -replace "(`"[^;]+?)`r`n",'$1') -split "`r`n" | Select -First 1).split($delimiter)).Count 
+   
+   $outputline = $outputline + " | " + ((Get-Content $File.FullName |Measure-object -line).lines ) + " | " + $numberOfColumns
    Write-output $outputline | out-file $output -append 
    
 }
